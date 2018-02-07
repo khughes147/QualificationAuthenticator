b725eb03d6bbc91d71df586c00cb97027569df42
B00658563 <khughes147@gmail.com>
2018-02-07 19:41:11 +0000

    Nav bar working

--- src/main/java/com/example/QualificationAuthenticator/QualificationAuthenticatorApplication.java
+++ src/main/java/com/example/QualificationAuthenticator/QualificationAuthenticatorApplication.java
@@ -1,9 +1,16 @@
 package com.example.QualificationAuthenticator;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
+
+@Controller
+@EnableAutoConfiguration
+@ComponentScan
 public class QualificationAuthenticatorApplication {
 
 	public static void main(String[] args) {
