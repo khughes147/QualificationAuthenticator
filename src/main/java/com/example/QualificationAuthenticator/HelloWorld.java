9d0c2286ce5303b57294ce559bbcfe4ac8dad32e
Hughes Kieran <B00658563@scm.net>
2018-02-07 16:36:13 +0000

    Initial commit

---
+++ src/main/java/com/example/QualificationAuthenticator/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.QualificationAuthenticator;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping("/helloworld")
+    public String index() {
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/example/QualificationAuthenticator/QualificationAuthenticatorApplication.java
@@ -0,0 +1,12 @@
+package com.example.QualificationAuthenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualificationAuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualificationAuthenticatorApplication.class, args);
+	}
+}
